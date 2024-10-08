404933d85fc501a088c6c41e38afaaed197b023d
dfdafhhssgdgfd <40103697@hughbaird.ac.uk>
2024-10-08 14:24:01 +0100

    Create a function to add a new student to the database

---
+++ Terry.py
@@ -0,0 +1,8 @@
+students = []
+
+name = input("Please input student name: ")
+age = input("Please input student age: ")
+grade = input("Please input student grade: ")
+
+def add_student(name, age, grade):
+    students.append([name, age, grade])
