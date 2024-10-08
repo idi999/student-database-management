7fb2911a04d7cece1f6b69440ad42f603ed86b1f
dfdafhhssgdgfd <40103697@hughbaird.ac.uk>
2024-10-08 15:48:43 +0100

    Update Terry.py

--- Terry.py
+++ Terry.py
@@ -1,8 +1,17 @@
 students = []
 
-name = input("Please input student name: ")
-age = input("Please input student age: ")
-grade = input("Please input student grade: ")
-
 def add_student(name, age, grade):
-    students.append([name, age, grade])
+
+    student_record = {
+        "name": name,
+        "age": age,
+        "grade": grade
+    }
+    students.append(student_record)
+    print(f"Added student: {student_record}")
+
+if __name__ == "__main__":
+    name = input("Please input student name: ")
+    age = input("Please input student age: ")
+    grade = input("Please input student grade: ")
+
