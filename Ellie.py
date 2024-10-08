988da0ae9c18d4b611cc217d5f0a6aeb00ac1f58
Ellie <124910652+LucidLiuxx@users.noreply.github.com>
2024-10-08 15:53:36 +0100

    Update Ellie.py

--- Ellie.py
+++ Ellie.py
@@ -1,5 +1,9 @@
-# Assuming the database is a 2D array named 'students'
+students = [
+    {'name' : 'John', 'age' : 18, 'grade' : 80.5}
+    {'name' : 'Samantha', 'age' : 20, 'grade' : 64.0}
+]
 
 def display_students():
     for i in range(len(students)):
         print(*students[i])
+
