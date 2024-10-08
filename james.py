a5379597574fe93d66a94550453879490628601f
JamesHD0902 <jameshushonday@gmail.com>
2024-10-08 15:24:55 +0100

    Update james.py

--- james.py
+++ james.py
@@ -1,4 +1,10 @@
-def delete_students(name):
-  student_database.pop(student_delete)
-  print(student_database)
-student_delete = input("What student would you like to delete?")
+def delete_student(name):
+    global student_database  
+
+    for student in student_database:
+        if student["name"].lower() == name.lower():  
+            student_database.remove(student) 
+            print(f"{name} has been deleted from the database.")
+            return
+        
+print(f"The student you have named {name} not found in the database.")
